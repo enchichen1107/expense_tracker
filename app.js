87bc846d07c6bad3d6c2ee22ce26fad3f81754a2
enchichen1107 <b03409032@gmail.com>
2021-06-14 21:36:52 +0800

    feat: add create record function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Record = require('./models/record')
 const app = express()
 const port = 3000
@@ -20,11 +21,14 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// setting template engine
+// set template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-// setting front page route
+// preprocess before entering routers
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// set front page route
 app.get('/', (req, res) => {
   Record.find()
     .lean()
@@ -32,7 +36,19 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
-// setting port
+// set create record route- show create page
+app.get('/records/create', (req, res) => {
+  return res.render('create')
+})
+
+// set create record route- post created data
+app.post('/records', (req, res) => {
+  return Record.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+// set port
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
 })
