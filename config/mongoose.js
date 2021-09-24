c9d040855a43d37582baccd54d2bd7e9e9208f05
enchichen1107 <b03409032@gmail.com>
2021-09-24 23:43:47 +0800

    feat: add demo pic to README

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 // set db connection
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/record-list'
 const mongoose = require('mongoose')
+
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', () => {
@@ -9,4 +10,5 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+
 module.exports = db
